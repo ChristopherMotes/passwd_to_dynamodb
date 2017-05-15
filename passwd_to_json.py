1f2f69120df0d7b94ea6e07f2fb2e342c909a32e
Christopher Motes <chris.motes@acumera.net>
2017-05-15 15:32:18 -0500

    converts json to list

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -2,8 +2,9 @@
 import os
 import json
 PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
-PasswdDict = {} 
+PasswdListofDicts = []
 for line in PasswdObject:
 	PasswdFields = line.strip().split(':')
-	PerLineDict = { "Name" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] }
-	print PerLineDict
+	PerLineDict = { "Name" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] , "GroupId" : PasswdFields[3] , "Comment" : PasswdFields[4] , "HomeDirectory" : PasswdFields[5] , "Shell" : PasswdFields[6] }
+	PasswdListofDicts.append(PerLineDict)
+print json.dumps(PasswdListofDicts)
