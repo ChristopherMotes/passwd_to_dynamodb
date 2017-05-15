e300dfa5d542dd5f85f3b9227e11305a00bfa40a
Christopher Motes <chris.motes@acumera.net>
2017-05-15 15:45:03 -0500

    non-structured data

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import os
 import json
+HOSTNAME = os.uname()[1]
 PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
 PasswdListofDicts = []
 for line in PasswdObject:
 	PasswdFields = line.strip().split(':')
-	PerLineDict = { "Name" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] , "GroupId" : PasswdFields[3] , "Comment" : PasswdFields[4] , "HomeDirectory" : PasswdFields[5] , "Shell" : PasswdFields[6] }
-	PasswdListofDicts.append(PerLineDict)
-print json.dumps(PasswdListofDicts)
+	PerLineDict = { "Hostname" : HOSTNAME , "UserName" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] , "GroupId" : PasswdFields[3] , "Comment" : PasswdFields[4] , "HomeDirectory" : PasswdFields[5] , "Shell" : PasswdFields[6] }
+	print json.dumps(PerLineDict)
