de2015a801a0fa6623d1b99bc3e5aebbd77d7739
Christopher Motes <chris.motes@acumera.net>
2017-05-15 17:22:04 -0500

    removed comment line, complex to script around missing comment, and field is utterly useless

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -8,5 +8,5 @@ PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
 PasswdListofDicts = []
 for line in PasswdObject:
 	PasswdFields = line.strip().split(':')
-	PerLineDict = { "Hostname" : {"S" : HOSTNAME } , "UserName" : { "S" : PasswdFields[0] } , "Password" : { "S" : PasswdFields[1] } , "UserId" : { "S" : PasswdFields[2] } , "GroupId"  : { "S" : PasswdFields[3] } , "Comment" : { "S" : PasswdFields[4] } , "HomeDirectory" : { "S" : PasswdFields[5] } , "Shell" : { "S" : PasswdFields[6] } }
+	PerLineDict = { "Hostname" : {"S" : HOSTNAME } , "UserName" : { "S" : PasswdFields[0] } , "Password" : { "S" : PasswdFields[1] } , "UserId" : { "S" : PasswdFields[2] } , "GroupId"  : { "S" : PasswdFields[3] } , "HomeDirectory" : { "S" : PasswdFields[5] } , "Shell" : { "S" : PasswdFields[6] } }
 	print json.dumps(PerLineDict)
