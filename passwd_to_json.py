7a21f58083949a198ef3453b42207382e29f843a
Christopher Motes <chris.motes@acumera.net>
2017-05-15 17:06:30 -0500

    destructure format complete

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 import os
 import json
+import boto3
+
 HOSTNAME = os.uname()[1]
 PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
 PasswdListofDicts = []
 for line in PasswdObject:
 	PasswdFields = line.strip().split(':')
-	PerLineDict = { "Hostname" : HOSTNAME , "UserName" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] , "GroupId" : PasswdFields[3] , "Comment" : PasswdFields[4] , "HomeDirectory" : PasswdFields[5] , "Shell" : PasswdFields[6] }
+	PerLineDict = { "Hostname" : {"S" : HOSTNAME } , "UserName" : { "S" : PasswdFields[0] } , "Password" : { "S" : PasswdFields[1] } , "UserId" : { "S" : PasswdFields[2] } , "GroupId"  : { "S" : PasswdFields[3] } , "Comment" : { "S" : PasswdFields[4] } , "HomeDirectory" : { "S" : PasswdFields[5] } , "Shell" : { "S" : PasswdFields[6] } }
 	print json.dumps(PerLineDict)
