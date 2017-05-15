9d15278aed44fc5e665da0c1e0976a9e84d8d07b
Christopher Motes <chris.motes@acumera.net>
2017-05-15 15:10:06 -0500

    building fields

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import os
 import json
-PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r') 
+PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
+PasswdDict = {} 
 for line in PasswdObject:
-	print line.strip().split(':')
+	PasswdFields = line.strip().split(':')
+	PerLineDict = { "Name" : PasswdFields[0] , "Password" : PasswdFields[1] , "UserId" : PasswdFields[2] }
+	print PerLineDict
