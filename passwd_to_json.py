505fce9cdac6ab5188a8dd1d9f0d06d427a5263b
Christopher Motes <chris.motes@acumera.net>
2017-05-16 09:32:47 -0500

    added ttl value

--- passwd_to_json.py
+++ passwd_to_json.py
@@ -6,14 +6,16 @@ import time
 import calendar
 boto3.setup_default_session(profile_name='motes-acumera')
 TIME_SINCE_EPOCH = calendar.timegm(time.gmtime())
-TTL = TIME_SINCE_EPOCH * 10 * 60
+TTL = 10 * 60
+TTL += TIME_SINCE_EPOCH
+VALUE = str(TTL)
 dynamodb_clinet = boto3.client('dynamodb')
 HOSTNAME = os.uname()[1]
 PasswdObject = open("/Users/cmotes/python/passwd_to_json/passwd", 'r')
 PasswdListofDicts = []
 for line in PasswdObject:
 	PasswdFields = line.strip().split(':')
-	PerLineDict = { "Hostname" : {"S" : HOSTNAME } , "UserName" : { "S" : PasswdFields[0] } , "Password" : { "S" : PasswdFields[1] } , "UserId" : { "S" : PasswdFields[2] } , "GroupId"  : { "S" : PasswdFields[3] } , "HomeDirectory" : { "S" : PasswdFields[5] } , "Shell" : { "S" : PasswdFields[6] } , "TTL" : { "N" : TTL } }
+	PerLineDict = { "Hostname" : {"S" : HOSTNAME } , "UserName" : { "S" : PasswdFields[0] } , "Password" : { "S" : PasswdFields[1] } , "UserId" : { "S" : PasswdFields[2] } , "GroupId"  : { "S" : PasswdFields[3] } , "HomeDirectory" : { "S" : PasswdFields[5] } , "Shell" : { "S" : PasswdFields[6] } , "TTL" : { "N" : VALUE } }
 	dynamodb_clinet.put_item(
 		TableName = 'user_accounts',
 		Item = PerLineDict
